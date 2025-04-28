23f5d46d264aeb1a2c8c7e2eaf9f988c4a63410b
butzke mo <butzkemo@hs-albsig.de>
2025-04-28 14:05:32 +0200

    changes to Docker adding apt up²

---
+++ frontend/src/views/Object.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Footer from '@/components/Footer.vue';
+import HeroInventory from '@/components/HeroInventory.vue';
+import NavBar from '@/components/NavBar.vue';
+
+</script>
+
+<template>
+    <HeroInventory/>
+    <NavBar/>
+
+
+    
+    <Footer/>
+</template>
